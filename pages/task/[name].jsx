6d1f219ab19e8b161d4e0a27d1d96f82081ee17a
Akhil BG <83879587+akhilbhadrangirija@users.noreply.github.com>
2022-07-11 18:00:57 +0530

    Static props eg

--- pages/task/[name].jsx
+++ pages/task/[name].jsx
@@ -1,3 +1,10 @@
+export async function getServerSideProps(context) {
+  await console.log(context.query)
+  return {
+    props: {}
+  }
+}
+
 function Hello() {
   return (
     <div className="flex justify-center w-screen h-screen items-center text-3xl font-bold">
