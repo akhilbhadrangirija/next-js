8e9235e515623a1c53406ecc08d6855c187c5d27
Akhil BG <83879587+akhilbhadrangirija@users.noreply.github.com>
2022-07-15 15:52:08 +0530

    Dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,13 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+       </main>
 
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
